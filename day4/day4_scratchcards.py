30ac6be7c2f2c938d9ab44fab5e75b23c6b2fe63
Radu Gheorghe <radu.gheorghe@snyk.io>
2023-12-04 10:59:53 +0200

    day4-part 2

--- day4/day4_scratchcards.py
+++ day4/day4_scratchcards.py
@@ -1,21 +1,27 @@
 import re
+from collections import defaultdict
 
 
 def main():
     total_score = 0
+    scratchcards_won = defaultdict(int)
+
     with open('input.txt', 'r') as f:
         # Read the file line by line
         data = f.readlines()
 
-    for line in data:
+    for index, line in enumerate(data):
         card_info, numbers = line.strip().split(':', 1)
+        card_id = int(card_info.rsplit(' ', 1)[-1])
+        print(f'Card ID: {card_id}')
         winning_numbers, drawn_numbers = numbers.split('|', 1)
         winning_numbers = list(map(int, re.findall(r'\d+', winning_numbers)))
-        draw_numbers = list(map(int, re.findall(r'\d+', drawn_numbers)))
-        print(f'Card: {card_info}, winning numbers: {winning_numbers}, draw numbers: {draw_numbers}')
+        drawn_numbers = list(map(int, re.findall(r'\d+', drawn_numbers)))
+        print(f'Winning numbers: {winning_numbers}')
+        print(f'Drawn numbers: {drawn_numbers}')
         # Calculate how many drawn numbers match the winning numbers
-        matches = len(set(winning_numbers).intersection(set(draw_numbers)))
-        print(f'Matches: {set(winning_numbers).intersection(set(draw_numbers))}; total: {matches}')
+        matches = len(set(winning_numbers).intersection(set(drawn_numbers)))
+        print(f'Matches: {set(winning_numbers).intersection(set(drawn_numbers))}; total: {matches}')
 
         # Calculate the score. The first match makes the card worth one point
         # and each match after the first doubles the point value of that card.
@@ -24,11 +30,20 @@ def main():
             score += 1
         for i in range(1, matches):
             score *= 2
-        print(f'Score: {score}\n')
-
+        print(f'Score: {score}')
         total_score += score
 
+        # Calculate how many scratchcards are won, based on previously won scratchcards
+        scratchcards_won[index] += 1
+        for j in range(index + 1, index + matches + 1):
+            scratchcards_won[j] += scratchcards_won[index]
+
+        print(f'Scratchcards won: {scratchcards_won}')
+
+        print('\n')
+
     print(f'Total score: {total_score}')
+    print(f'Total scratchcards won: {sum([scratchcard for scratchcard in scratchcards_won.values()])}')
 
 
 if __name__ == '__main__':
